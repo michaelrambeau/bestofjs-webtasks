49346db201048d8969360e56c42a966f7693b494
Michael Rambeau <mikeair@gmail.com>
2016-02-10 06:27:01 +0900

    Update test sequence: cleanup => setup => API tests

--- commands/cleanup.js
+++ commands/cleanup.js
@@ -6,6 +6,15 @@ const cleanup = require('../test/db/cleanup');
 const connect = require('../test/db/connect');
 
 connect(process.env.MONGO_URI_TEST)
-  .then(db => cleanup(db))
-  .then(db => db.close())
-  .then(() => console.log('Cleanup finish!'));
+.then(db => {
+  return cleanup(db)
+    .then(() => db.close())
+    .catch(err => {
+      db.close();
+      console.log('Error during the cleanup', err.message);
+      throw new Error(err.message);
+    });
+})
+.then(() => console.log('Cleanup OK!'))
+// exit with an error code to prevent the tests from running if setup fails
+.catch(() => process.exit(1));
