5fcf71e0db9ed9a1ba2c826218dd0a1a81636539
Michael Rambeau <mikeair@gmail.com>
2016-04-19 06:59:00 +0900

    Update sample data `_id` field using mongodb ObjectId(), to be able to edit data.

--- test/sample/links.js
+++ test/sample/links.js
@@ -4,17 +4,20 @@ module.exports = {
   "results": [
     {
       "_id": ObjectId("56ad8b90f6a3e89c08b88ca2"),
-      "title": "link",
+      "title": "ES6 promises in depth",
       "url": "https://ponyfoo.com/articles/es6-promises-in-depth",
       "projects": [
         "55723c9f4140883353bc773e"
       ],
+      "comment": {
+        "md": "Everything you need to know about promises."
+      },
       "createdBy": "usertest1",
       "createdAt": "2016-01-31T04:20:32.186Z"
     },
     {
       "_id": ObjectId("56ad80027703eee4132d48e2"),
-      "title": "link2",
+      "title": "My link about 4 projects",
       "url": "http://development.bestofjs.divshot.io",
       "projects": [
         "56a95b5843bdc81100111331",
@@ -24,7 +27,9 @@ module.exports = {
       ],
       "createdBy": "michaelrambeau",
       "createdAt": "2016-01-31T03:31:14.572Z",
-      "comment": "aaa\nbbbb"
+      "comment": {
+        "md": "This is a link about 4 projects:\n\n* preact\n* React Native\n* ..."
+      }
     },
     {
       "_id": ObjectId("56adf53426d6e7181453e1bb"),
@@ -35,7 +40,9 @@ module.exports = {
       ],
       "createdBy": "michaelrambeau",
       "createdAt": "2016-01-31T11:51:16.132Z",
-      "comment": "yeah!"
+      "comment": {
+        "md": "I am going to tell you why Bootstrap is cool."
+      }
     }
   ]
 };
--- test/sample/reviews.js
+++ test/sample/reviews.js
@@ -1,20 +1,26 @@
+const mongodb = require('mongodb');
+const ObjectId = mongodb.ObjectId;
 module.exports = {
   "results": [
     {
-      "_id": "56ae781dad54d8381af193f2",
+      "_id": ObjectId("56ae781dad54d8381af193f2"),
       "project": "55723c9f4140883353bc773e",
       "rating": 4,
       "createdBy": "michaelrambeau",
       "createdAt": "2016-01-31T21:09:49.136Z",
-      "comment": "This is my comment: good!"
+      "comment": {
+        "md": "This is my comment: good!"
+      }
     },
     {
-      "_id": "56b25fedacc5b3640932d68e",
+      "_id": ObjectId("56b25fedacc5b3640932d68e"),
       "project": "56a95b5843bdc81100111331",
       "rating": 5,
       "createdBy": "michaelrambeau",
       "createdAt": "2016-02-03T20:15:41.567Z",
-      "comment": "Here is an other comment: great!"
+      "comment": {
+        "md": "Here is an other comment: great!"
+      }
     }
   ]
 }
