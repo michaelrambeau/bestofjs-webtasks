a69a13bd9e7ea099d1b7a80f75719bc25fb45b16
Michael Rambeau <mikeair@gmail.com>
2016-04-23 08:35:39 +0900

    Add cache-control max-age header

--- user-content-api/src/lib/app.js
+++ user-content-api/src/lib/app.js
@@ -199,6 +199,7 @@ function crossDomainMiddleware(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type,token');
+  res.header('Cache-Control', 'max-age=7200');
   next();
 }
 
